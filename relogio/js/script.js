0ed2307f87ff1ba76ef4e7475aca5efe8e1a7a2b
KaiioBr <kaiomae8@gmail.com>
2025-10-01 19:39:50 +0000

    save

---
+++ relogio/js/script.js
@@ -0,0 +1,38 @@
+//DOM
+
+const horas = document.querySelector('#horas')
+const minutos = document.querySelector('#minutos')
+const segundos = document.querySelector('#segundos')
+
+// Eventos
+
+setInterval(relogio, 1000)
+
+// funçoes
+
+function relogio(){
+    
+    hoje = new Date()
+
+    hr = hoje.getHours()
+    min = hoje.getMinutes()
+    seg = hoje.getSeconds()
+
+    horas.textContent = hr
+    minutos.textContent = min
+    segundos.textContent = seg
+}
+
+if(hr<10){
+    hr = '0'+hr
+}
+if(min<10){
+    min = '0'+min
+}
+ if(seg<10){
+    seg = '0'+seg
+}
+
+horas.textContent = hr
+minutos.textContent = min
+segundos.textContent = seg
