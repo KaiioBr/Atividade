6afb2b482a1eb9d3fdd1622e7d427d5f2ee1e05c
KaiioBr <kaiomae8@gmail.com>
2025-11-26 19:12:16 +0000

    save

---
+++ tempo/js/script.js
@@ -0,0 +1,15 @@
+//DOM
+const cidade = document.querySelector('#cidade')
+const bprocurar = document.querySelector('#bprocurar')
+const city = document.querySelector('#city')
+const temperatura = document.querySelector('#temperatura')
+const previsao = document.querySelector('#previsao')
+const chave = '79f5fbb8ba7a2f23cfd1c77e29e825ee'
+
+//EVENT
+
+bprocurar.addEventListener('click', buscarcidade)
+
+
+
+//FUNCTION
